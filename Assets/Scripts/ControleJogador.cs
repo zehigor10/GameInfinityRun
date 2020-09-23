eef958b2553f96983bfe6311c265ee06b41dd76d
zehigor10 <jhigorfoliveira@gmail.com>
2020-09-23 02:44:11 -0300

    subindo versão final

--- Assets/Scripts/ControleJogador.cs
+++ Assets/Scripts/ControleJogador.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ControleJogador : MonoBehaviour
 {
@@ -13,17 +14,33 @@ public class ControleJogador : MonoBehaviour
 
     private Vector3 target;
 
-    private Vector2 initialPosition; 
+    private Vector2 initialPosition;
+
+    public GameObject chao;
+    public GameObject cube;
+    public GameObject moeda;
+    // public text txtGameOver;
+    // public Text txtPontos;
+
+    private int estagioAtual = 1;
+
+    private int pontos = 0;
 
     void Start()
     {
         raiaAtual = 1;
-        target = jogador.transform.position; 
+        target = jogador.transform.position;
+        montarCenario();
+        // txtPontos.text = "" + pontos;
     }
+
+
 
     void Update()
 
     {
+        transform.Rotate(new Vector3(0, 90, 0) * Time.deltaTime);
+
         // x-> lateral, y-> altura, z->profundida
         int novaRaia = -1;
 
@@ -59,11 +76,12 @@ public class ControleJogador : MonoBehaviour
         // touch
         if (Input.touchCount >= 1)
         {
-            if(Input.GetTouch(0).phase == TouchPhase.Began)
+            if (Input.GetTouch(0).phase == TouchPhase.Began)
             {
                 initialPosition = Input.GetTouch(0).position;
 
-            }else if (Input.GetTouch(0).phase == TouchPhase.Ended || Input.GetTouch(0).phase == TouchPhase.Canceled)
+            }
+            else if (Input.GetTouch(0).phase == TouchPhase.Ended || Input.GetTouch(0).phase == TouchPhase.Canceled)
             {
                 if (Input.GetTouch(0).position.x > initialPosition.x && raiaAtual < 2)
                 {
@@ -73,12 +91,12 @@ public class ControleJogador : MonoBehaviour
                 {
                     novaRaia = raiaAtual - 1;
                 }
-                    {
+                {
 
-                    }
                 }
             }
-            
+        }
+
         if (novaRaia >= 0)
         {
             raiaAtual = novaRaia;
@@ -89,9 +107,114 @@ public class ControleJogador : MonoBehaviour
         {
             jogador.transform.position = Vector3.Lerp(jogador.transform.position, target, 5 * Time.deltaTime);
         }
- 
- 
+
+
         //mover o chão 
         cenario.transform.Translate(0, 0, velocidadeCenario * Time.deltaTime * -1);
+
+        float estagio = Mathf.Floor(((cenario.transform.position.z - 50.0F) / -100.0F)) + 1;
+        if (estagio > estagioAtual)
+        {
+            GameObject chao2 = Instantiate(chao);
+            float chao2z = ((100 * estagioAtual) + 50) + cenario.transform.position.z;
+            chao2.transform.SetParent(cenario.transform);
+            chao2.transform.position = new Vector3(chao.transform.position.x, chao.transform.position.y, chao2z);
+            estagioAtual++;
+            montarCenario();
+        }
+
+    }
+
+    void montarCenario()
+    {
+        for (int i = 2; i < 10; i++)
+        {
+            int elemento1 = Random.Range(0, 3);
+            int elemento2 = Random.Range(0, 3);
+            int elemento3 = Random.Range(0, 3);
+            //0 = nada / 1 = cubo / 2=moeda
+            if (elemento1 == 1 && elemento2 == 1 && elemento3 == 1)
+            {
+                elemento2 = 0;
+            }
+            if (elemento1 == 1)
+            {
+                instanciarCubo(i, 0);
+            }
+            else if (elemento1 == 2)
+            {
+                instanciarMoeda(i, 0);
+            }
+            if (elemento2 == 1)
+            {
+                instanciarCubo(i, 1);
+            }
+            else if (elemento2 == 2)
+            {
+                instanciarMoeda(i, 1);
+            }
+            if (elemento3 == 1)
+            {
+                instanciarCubo(i, 2);
+            }
+            else if (elemento3 == 2)
+            {
+                instanciarMoeda(i, 2);
+            }
+        }
+    }
+
+    void instanciarCubo(int posicaoz, int posicaox)
+    {
+        GameObject cubo2 = Instantiate(cube);
+        float posz = ((10 * posicaoz) + ((estagioAtual - 1) * 100)) +
+        cenario.transform.position.z;
+        float posx = (posicaox - 1) * distanciaRaia;
+        cubo2.transform.SetParent(cenario.transform);
+        cubo2.transform.position = new Vector3(posx, 2F, posz);
+    }
+
+    void instanciarMoeda(int posicaoz, int posicaox)
+    {
+        GameObject moeda2 = Instantiate(moeda);
+        float posz = ((10 * posicaoz) + ((estagioAtual - 1) * 100)) +
+        cenario.transform.position.z;
+        float posx = (posicaox - 1) * distanciaRaia;
+        moeda2.transform.SetParent(cenario.transform);
+        moeda2.transform.position = new Vector3(posx, 0.5F, posz);
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.CompareTag("Moeda"))
+        {
+            Destroy(col.gameObject);
+        }
+        if (col.gameObject.CompareTag("Obstaculo"))
+        {
+            SceneManager.LoadScene("GameOver", LoadSceneMode.Single);
+        }
+    }
+
+}
+/*
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.CompareTag("Moeda"))
+        {
+            Destroy(col.gameObject);
+            pontos++;
+            txtPontos.text = "" + pontos;
+
+        }
+        if (col.gameObject.CompareTag("Obstaculo"))
+        {
+            txtGameOver.text = "GAME OVER";
+            isGameOver = true;
+            invoke("GameOver", 5); 
+        }
     }
 }
+*/ 
+
+  
---
+++ Assets/Scripts/Moeda.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class Moeda : MonoBehaviour
+{
+    void Start()
+    {
+    }
+    void Update()
+    {
+        transform.Rotate(new Vector3(0, 0, 90) * Time.deltaTime);
+    }
+}
